20eed15cfa3d79f6d1de788ac1efe83c84fb25d1
HarshalBhogawade <harshalbhogawade1@gmail.com>
2025-11-07 21:07:23 +0530

    Fix: Add allow_unsafe_werkzeug=True for Render production deployment

--- application.py
+++ application.py
@@ -172,4 +172,4 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') == 'development'
-    socketio.run(app, debug=debug, host='0.0.0.0', port=port)
+    socketio.run(app, debug=debug, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
